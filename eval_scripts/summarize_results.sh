50a837c6eb1955d54decb342f5c786cc8e5ef92c
Hume2 <teratux.mail@gmail.com>
2019-07-08 14:48:52 +0200

    summarize_results.sh: Fix for Ubuntu 18.04

--- eval_scripts/summarize_results.sh
+++ eval_scripts/summarize_results.sh
@@ -55,12 +55,14 @@ do
 			indmin=$order
 		fi
 	done
-	grep Overall $REPORTS/$model\_$indmin* |sed s/.*$model.$indmin.//|sed s/.txt.Overall.//|sort -n|cut -f 1,3,4 -d ' '|awk '{print $1,$2+$3}' >$model.txt
+	grep -ir Overall $REPORTS/$model\_$indmin* |sed s/.*$model.$indmin.//|sed s/.txt.Overall.//|sort -n|cut -f 1,3,4 -d ' '|awk '{print $1,$2+$3}' >$model.txt
 	echo Model $model param $indmin has $errmin error.  >>best.txt
 done
 create_graph |dot -Tpdf >$d.pdf
 
-convert -density 200 $d.pdf -trim -bordercolor white $d.png 
+#convert -density 200 $d.pdf -trim -bordercolor white $d.png 
+pdftoppm $d.pdf $d -png -r 200
+convert $d-1.png -trim -bordercolor white $d.png
 extend_figure $d.png
 cat best.txt |cut -f 2,4 -d ' '|tr ' ' _|sed s/$/.txt/|sed s/^/..\\/results\\/$d\\//
 f=0
@@ -74,7 +76,9 @@ do
 done
 gnuplot draw_summary.gnu >graphs.fig
 fig2dev -Lpdf graphs.fig graphs.pdf
-convert -density 200 graphs.pdf -trim -resize 500x400 graphs.png
+#convert -density 200 graphs.pdf -trim -resize 500x400 graphs.png
+pdftoppm graphs.pdf graphs -png -r 200
+convert graphs-1.png -trim -resize 500x400 graphs.png
 extend_figure graphs.png 
 convert -size 900x450 xc:white \
 	-draw 'Image src-over 25,50 500,400 'graphs.png'' \
